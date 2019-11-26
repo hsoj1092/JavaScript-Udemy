19c273d8d712469d8034e1cbc7c31387bddd08ad
hsoj1092 <josh1092@gmail.com>
2019-11-26 17:13:05 -0500

    Commit 11 26 19

--- Section2/Fifth Challenge/CC5.js
+++ Section2/Fifth Challenge/CC5.js
@@ -1,25 +1,24 @@
-var bills = [124,48,268];
+var bills = [124,48,268,180,42];
 var tips = [];  //empty list
 var billwTips = []; //empty list
+var  n = 0;
+var bwt = 0;
 function tipCalc(bill){
     for(var i=0;i<bill.length;i++){
         if(bill[i]<50){
-            var  n = bill[i]*.20       //calculates tip set to n
-            n = Number(n.toFixed(2));  //this keeps the number to 2 decimal places. tofixed creates a string but Number makes it a number again :/
+            n = Number((bill[i]*.20).toFixed(2));       //calculates tip set to n //this keeps the number to 2 decimal places. tofixed creates a string but Number makes it a number again :/
             tips.push(n);              // adds it to the empty list     
-            var bwt=n+bill[i];         //adds tip set as n and the current bill at bill[i]
+            bwt=n+bill[i];         //adds tip set as n and the current bill at bill[i]
             billwTips.push(bwt);       // adds bill with tip to new array
         }else if(bill[i]>=50 && bill[i]<200){
-            var  n = bill[i]*.15
-            n = Number(n.toFixed(2));
+            n = Number((bill[i]*.15).toFixed(2));
             tips.push(n);
-            var bwt=n+bill[i];
+            bwt=n+bill[i];
             billwTips.push(bwt);
         }else if(bill[i]>=200){
-            var  n = bill[i]*.10
-            n = Number(n.toFixed(2));
+            n = Number((bill[i]*.10).toFixed(2));
             tips.push(n);
-            var bwt=n+bill[i];
+            bwt=n+bill[i];
             billwTips.push(bwt); 
         }
     }   
--- Section2/Fourth Challenge/CC4.js
+++ Section2/Fourth Challenge/CC4.js
@@ -1,10 +1,27 @@
-var john = {
-    height : 1.6,
-    weight : 96,
-    bmi : 2
+var John = {
+    name: 'John Smith',
+    height : 1.4,
+    mass : 51,
+    calcBmi: function(){
+        this.bmi = (this.mass/(this.height*this.height)).toFixed(2);
+    }
 };
-var mike = {
-    height : 1.6,
-    weight : 96,
-    bmi : 2
+John.calcBmi();
+var Mark = {
+    name: 'Mark Miller',
+    height : 1.4,
+    mass : 52,
+    calcBmi: function(){
+        this.bmi = (this.mass/(this.height*this.height)).toFixed(2);
+    }
+}; 
+Mark.calcBmi();
+
+if(John.bmi > Mark.bmi){
+    console.log(John.name + " has a higher BMI index of "+ John.bmi)
+}else if (Mark.bmi > John.bmi){
+    console.log(Mark.name +" has a higher BMI index of "+ Mark.bmi)
+}else{
+    console.log("John and Mark have the same BMI index of "+ John.bmi)
 };
+
